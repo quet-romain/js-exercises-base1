f069f493124df346c3f5c161ab8fcc5d4c870382
quet-romain <quet.romain@gmail.com>
2016-11-07 16:07:50 +0100

    exo fini

---
+++ Exercice_6.js
@@ -0,0 +1,6 @@
+function result() {
+    var num1 = document.getElementById("premier_nombre").value;
+    var num2 = document.getElementById("deuxieme_nombre").value;
+    
+    alert(num1 % num2)
+}
